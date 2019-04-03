f6c8622458d0fabb15282437712583294ef9edce
[ John Vant - Unknown - Graduate Student - asinghar ] <jvant@agave1.agave.rc.asu.edu>
2019-04-02 20:21:33 -0700

    Dhanged batch submission script

--- submit/step1.sh
+++ submit/step1.sh
@@ -5,7 +5,8 @@
 #SBATCH -p asinghargpu1
 #SBATCH -q asinghargpu1
 #SBATCH --gres=gpu:GTX1080:1
+#SBATCH -o ./slurm
 
 module load namd/2.13b1-cuda
 
-namd2 +p16 ./config/Mdff-step1.namd > ./output/Mdff-step1.log
+namd2 +p16 ../config/Mdff-step1.namd > ../output/Mdff-step1.log
