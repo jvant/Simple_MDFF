662844fee636406c5c1703cda8a92464cbc2677c
jvant <jvant@asu.edu>
2019-04-02 19:55:07 -0700

    First Commit

---
+++ submit/step1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -N 3
+#SBATCH -n 24
+#SBATCH -p asinghargpu1
+#SBATCH -q asinghargpu1
+#SBATCH --gres=gpu:GTX1080:1
+
+module load namd/2.13b1-cuda
+
+namd2 +p16 ./config/Mdff-step1.namd > ./output/Mdff-step1.log
